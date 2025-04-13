b275f978d4da4cb665c086826bee38cb1a3d2405
Ishita <ishita.23bcs10188@ms.sst.scaler.com>
2025-04-13 17:30:22 +0530

    added final changes for deployment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import userRouter from './routes/user.route.js';
 import eventRouter from './routes/event.route.js';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -20,6 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT;
 app.use(express.json());
 
+const _dirname = path.resolve(); 
+
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
@@ -30,6 +33,11 @@ app.use('/server/auth', userRouter);
 
 app.use('/server/event', eventRouter);
 app.use('/uploads', express.static('uploads'));
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
+app.get(/^\/(?!server\/|uploads\/).*/, (_, res) => {
+    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'));
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}` );
